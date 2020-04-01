eb91b821462b00d553c9e0c89a3c108c2a0202ca
Michael Stallings <stall84@gmail.com>
2020-04-01 16:49:29 -0400

    first commit of various practice files for Node.js

---
+++ logger.js
@@ -0,0 +1,12 @@
+// var url = 'http://mylogger.io/log';
+
+// function log (message) {
+//     console.log(message);
+// }
+
+// module.exports = log;
+
+var myArgs = process.argv.slice(2);
+
+console.log('my args: ', myArgs);
+
